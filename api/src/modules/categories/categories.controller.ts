97db71850c2d7d2ea85adba0eae72952bcf4807f
Daniel Vidal <danielsv465@hotmail.com>
2023-07-18 10:00:24 -0300

    categories module

---
+++ api/src/modules/categories/categories.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ActiveUserId } from 'src/shared/decorators/active-user-id.decorator';
+import { CategoriesService } from './categories.service';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll(@ActiveUserId() userId: string) {
+    return this.categoriesService.findAllByUserId(userId);
+  }
+}
