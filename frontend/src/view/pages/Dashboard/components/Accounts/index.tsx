831288ac174600792a41ad243ff9f071068ce6f1
Daniel Vidal <danielsv465@hotmail.com>
2024-01-08 14:06:51 -0300

    Accounts/Transactions load

--- frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { cn } from '../../../../../app/utils/cn';
 import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { Spinner } from '../../../../../components/Spinner';
 import { EyeIcon } from '../../../../../components/icons/EyeIcon';
 import { AccountCard } from './AccountCard';
 import { SliderNavigation } from './SliderNavigation';
@@ -15,85 +16,96 @@ export function Accounts() {
     windowWidth,
     areValuesVisible,
     toggleValuesVisibility,
+    isLoading,
   } = useAccountsController();
 
   return (
     <div className="bg-teal-900 rounded-2xl w-full h-full md:p-10 px-4 py-8 flex flex-col">
-      <div>
-        <span className="tracking-[-0.5px] text-white block">
-          Saldo total
-        </span>
-
-        <div className="flex items-center gap-2">
-          <strong
-            className={cn(
-              'text-2xl tracking-[-1px] text-white',
-              !areValuesVisible && 'blur-md',
-            )}
-          >
-            {formatCurrency(1000)}
-          </strong>
-
-          <button
-            className="w-8 h-8 flex items-center justify-center"
-            onClick={toggleValuesVisibility}
-          >
-            <EyeIcon open={!areValuesVisible} />
-          </button>
+      {isLoading && (
+        <div className="w-full h-full flex items-center justify-center">
+          <Spinner className="text-teal-950/50 fill-white w-10 h-10"/>
         </div>
-      </div>
+      )}
 
-      <div className="flex-1 flex flex-col justify-end mt-10 md:mt0">
-        <div>
-          <Swiper
-            spaceBetween={16}
-            slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
-            onSlideChange={(swiper) => {
-              setSliderState({
-                isBeginning: swiper.isBeginning,
-                isEnd: swiper.isEnd,
-              });
-            }}
-          >
+      {!isLoading && (
+        <>
+          <div>
+            <span className="tracking-[-0.5px] text-white block">
+              Saldo total
+            </span>
 
-            <div className="flex items-center justify-between mb-4" slot="container-start">
-              <strong className="text-white tracking-[-1px] text-lg font-bold">
-                Minhas contas
+            <div className="flex items-center gap-2">
+              <strong
+                className={cn(
+                  'text-2xl tracking-[-1px] text-white',
+                  !areValuesVisible && 'blur-md',
+                )}
+              >
+                {formatCurrency(1000)}
               </strong>
 
-              <SliderNavigation
-                isBeginning={sliderState.isBeginning}
-                isEnd={sliderState.isEnd}
-              />
+              <button
+                className="w-8 h-8 flex items-center justify-center"
+                onClick={toggleValuesVisibility}
+              >
+                <EyeIcon open={!areValuesVisible} />
+              </button>
             </div>
+          </div>
 
-            <SwiperSlide>
-              <AccountCard
-                color="#7950f2"
-                name="Nubank"
-                balance={1000.23}
-                type="CASH"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <AccountCard
-                color="#7950f2"
-                name="Nubank"
-                balance={1000.23}
-                type="CHECKING"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <AccountCard
-                color="#7950f2"
-                name="Nubank"
-                balance={1000.23}
-                type="INVESTMENT"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
+          <div className="flex-1 flex flex-col justify-end mt-10 md:mt0">
+            <div>
+              <Swiper
+                spaceBetween={16}
+                slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
+                onSlideChange={(swiper) => {
+                  setSliderState({
+                    isBeginning: swiper.isBeginning,
+                    isEnd: swiper.isEnd,
+                  });
+                }}
+              >
+
+                <div className="flex items-center justify-between mb-4" slot="container-start">
+                  <strong className="text-white tracking-[-1px] text-lg font-bold">
+                    Minhas contas
+                  </strong>
+
+                  <SliderNavigation
+                    isBeginning={sliderState.isBeginning}
+                    isEnd={sliderState.isEnd}
+                  />
+                </div>
+
+                <SwiperSlide>
+                  <AccountCard
+                    color="#7950f2"
+                    name="Nubank"
+                    balance={1000.23}
+                    type="CASH"
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <AccountCard
+                    color="#7950f2"
+                    name="Nubank"
+                    balance={1000.23}
+                    type="CHECKING"
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <AccountCard
+                    color="#7950f2"
+                    name="Nubank"
+                    balance={1000.23}
+                    type="INVESTMENT"
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { MONTHS } from '../../../../../app/config/constants';
 import { cn } from '../../../../../app/utils/cn';
 import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { Spinner } from '../../../../../components/Spinner';
 import { FilterIcon } from '../../../../../components/icons/FilterIcon';
 import { TransactionsIcon } from '../../../../../components/icons/TransactionsIcon';
 import { CategoryIcon } from '../../../../../components/icons/categories/CategoryIcon';
@@ -12,95 +13,105 @@ import { SliderOption } from './SliderOption';
 import { useTransactionsController } from './useTransactionsController';
 
 export function Transactions() {
-  const { areValuesVisible } = useTransactionsController();
+  const { areValuesVisible, isLoading } = useTransactionsController();
 
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
-      <header>
-        <div className="flex items-center justify-between">
-          <button className="flex items-center gap-2">
-            <TransactionsIcon />
-            <span  className="text-sm text-gray-800 tracking-[-0.5px] font-medium">
-              Transações
-            </span>
-            <ChevronDownIcon className="text-gray-900"/>
-          </button>
-
-          <button>
-            <FilterIcon />
-          </button>
+      {isLoading && (
+        <div className="w-full h-full flex items-center justify-center">
+          <Spinner className="w-10 h-10"/>
         </div>
+      )}
 
-        <div className="mt-6 relative">
-          <Swiper
-            slidesPerView={3}
-            centeredSlides
-          >
-            <SliderNavigation />
-            {MONTHS.map((month, index) => (
-              <SwiperSlide key={month}>
-                {({ isActive }) => (
-                  <SliderOption
-                    isActive={isActive}
-                    month={month}
-                    index={index}
-                  />
+      {!isLoading && (
+        <>
+          <header>
+            <div className="flex items-center justify-between">
+              <button className="flex items-center gap-2">
+                <TransactionsIcon />
+                <span  className="text-sm text-gray-800 tracking-[-0.5px] font-medium">
+                  Transações
+                </span>
+                <ChevronDownIcon className="text-gray-900"/>
+              </button>
+
+              <button>
+                <FilterIcon />
+              </button>
+            </div>
+
+            <div className="mt-6 relative">
+              <Swiper
+                slidesPerView={3}
+                centeredSlides
+              >
+                <SliderNavigation />
+                {MONTHS.map((month, index) => (
+                  <SwiperSlide key={month}>
+                    {({ isActive }) => (
+                      <SliderOption
+                        isActive={isActive}
+                        month={month}
+                        index={index}
+                      />
+                    )}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </header>
+
+          <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+              <div className="flex-1 flex items-center gap-3">
+                <CategoryIcon type="expense"/>
+
+                <div>
+                  <strong className="font-bold tracking-[-0.5px] block">
+                    Almoço
+                  </strong>
+                  <span className="text-sm text-gray-600">
+                    10/10/1995
+                  </span>
+                </div>
+              </div>
+
+              <span
+                className={cn(
+                  'text-red-800 tracking-[-0.5px] font-medium',
+                  !areValuesVisible && 'blur-sm',
                 )}
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </header>
+              >
+                - {formatCurrency(1200)}
+              </span>
+            </div>
 
-      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-          <div className="flex-1 flex items-center gap-3">
-            <CategoryIcon type="expense"/>
+            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+              <div className="flex-1 flex items-center gap-3">
+                <CategoryIcon type="income"/>
 
-            <div>
-              <strong className="font-bold tracking-[-0.5px] block">
-                Almoço
-              </strong>
-              <span className="text-sm text-gray-600">
-                10/10/1995
+                <div>
+                  <strong className="font-bold tracking-[-0.5px] block">
+                    Salario
+                  </strong>
+                  <span className="text-sm text-gray-600">
+                    10/10/1995
+                  </span>
+                </div>
+              </div>
+
+              <span
+                className={cn(
+                  'text-green-800 tracking-[-0.5px] font-medium',
+                  !areValuesVisible && 'blur-sm',
+                )}
+              >
+                + {formatCurrency(1200)}
               </span>
             </div>
           </div>
-
-          <span
-            className={cn(
-              'text-red-800 tracking-[-0.5px] font-medium',
-              !areValuesVisible && 'blur-sm',
-            )}
-          >
-            - {formatCurrency(1200)}
-          </span>
-        </div>
-
-        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-          <div className="flex-1 flex items-center gap-3">
-            <CategoryIcon type="income"/>
-
-            <div>
-              <strong className="font-bold tracking-[-0.5px] block">
-                Salario
-              </strong>
-              <span className="text-sm text-gray-600">
-                10/10/1995
-              </span>
-            </div>
-          </div>
-
-          <span
-            className={cn(
-              'text-green-800 tracking-[-0.5px] font-medium',
-              !areValuesVisible && 'blur-sm',
-            )}
-          >
-            + {formatCurrency(1200)}
-          </span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
