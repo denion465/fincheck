eefcbead9a9249ac1097da56f3655b785f380955
Daniel Vidal <danielsv465@hotmail.com>
2024-01-08 16:20:58 -0300

    Transactions loading filters

--- frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -1,4 +1,6 @@
 import 'swiper/css';
+
+import { PlusIcon } from '@radix-ui/react-icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { cn } from '../../../../../app/utils/cn';
@@ -17,6 +19,7 @@ export function Accounts() {
     areValuesVisible,
     toggleValuesVisibility,
     isLoading,
+    accounts,
   } = useAccountsController();
 
   return (
@@ -54,55 +57,80 @@ export function Accounts() {
           </div>
 
           <div className="flex-1 flex flex-col justify-end mt-10 md:mt0">
-            <div>
-              <Swiper
-                spaceBetween={16}
-                slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
-                onSlideChange={(swiper) => {
-                  setSliderState({
-                    isBeginning: swiper.isBeginning,
-                    isEnd: swiper.isEnd,
-                  });
-                }}
-              >
-
-                <div className="flex items-center justify-between mb-4" slot="container-start">
+            {accounts.length === 0 && (
+              <>
+                <div className="mb-4" slot="container-start">
                   <strong className="text-white tracking-[-1px] text-lg font-bold">
                     Minhas contas
                   </strong>
-
-                  <SliderNavigation
-                    isBeginning={sliderState.isBeginning}
-                    isEnd={sliderState.isEnd}
-                  />
                 </div>
 
-                <SwiperSlide>
-                  <AccountCard
-                    color="#7950f2"
-                    name="Nubank"
-                    balance={1000.23}
-                    type="CASH"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <AccountCard
-                    color="#7950f2"
-                    name="Nubank"
-                    balance={1000.23}
-                    type="CHECKING"
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <AccountCard
-                    color="#7950f2"
-                    name="Nubank"
-                    balance={1000.23}
-                    type="INVESTMENT"
-                  />
-                </SwiperSlide>
-              </Swiper>
-            </div>
+                <button
+                  className="mt-4 h-52 rounded-2xl border-2 border-dashed border-teal-600 flex flex-col items-center justify-center gap-4 text-white"
+                >
+                  <div
+                    className="w-11 h-11 rounded-full border-2 border-dashed border-white flex items-center justify-center"
+                  >
+                    <PlusIcon className="w-6 h-6"/>
+                  </div>
+                  <span className="tracking-[-0.5px] font-medium block w-32 text-center">
+                    Cadastra uma nova conta
+                  </span>
+                </button>
+              </>
+            )}
+
+            {accounts.length > 0 && (
+              <div>
+                <Swiper
+                  spaceBetween={16}
+                  slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
+                  onSlideChange={(swiper) => {
+                    setSliderState({
+                      isBeginning: swiper.isBeginning,
+                      isEnd: swiper.isEnd,
+                    });
+                  }}
+                >
+
+                  <div className="flex items-center justify-between mb-4" slot="container-start">
+                    <strong className="text-white tracking-[-1px] text-lg font-bold">
+                      Minhas contas
+                    </strong>
+
+                    <SliderNavigation
+                      isBeginning={sliderState.isBeginning}
+                      isEnd={sliderState.isEnd}
+                    />
+                  </div>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#7950f2"
+                      name="Nubank"
+                      balance={1000.23}
+                      type="CASH"
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#7950f2"
+                      name="Nubank"
+                      balance={1000.23}
+                      type="CHECKING"
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#7950f2"
+                      name="Nubank"
+                      balance={1000.23}
+                      type="INVESTMENT"
+                    />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            )}
           </div>
         </>
       )}
--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { MONTHS } from '../../../../../app/config/constants';
 import { cn } from '../../../../../app/utils/cn';
 import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import emptyState from '../../../../../assets/empty-state.svg';
 import { Spinner } from '../../../../../components/Spinner';
 import { FilterIcon } from '../../../../../components/icons/FilterIcon';
 import { TransactionsIcon } from '../../../../../components/icons/TransactionsIcon';
@@ -13,17 +14,24 @@ import { SliderOption } from './SliderOption';
 import { useTransactionsController } from './useTransactionsController';
 
 export function Transactions() {
-  const { areValuesVisible, isLoading } = useTransactionsController();
+  const {
+    areValuesVisible,
+    isInitialLoading,
+    isLoading,
+    transactions,
+  } = useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
-      {isLoading && (
+      {isInitialLoading && (
         <div className="w-full h-full flex items-center justify-center">
           <Spinner className="w-10 h-10"/>
         </div>
       )}
 
-      {!isLoading && (
+      {!isInitialLoading && (
         <>
           <header>
             <div className="flex items-center justify-between">
@@ -62,53 +70,72 @@ export function Transactions() {
           </header>
 
           <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="expense"/>
+            {isLoading && (
+              <div className="flex flex-col h-full items-center justify-center">
+                <Spinner className="w-10 h-10"/>
+              </div>
+            )}
 
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">
-                    Almoço
-                  </strong>
-                  <span className="text-sm text-gray-600">
-                    10/10/1995
+            {(!hasTransactions && !isLoading) && (
+              <div className="flex flex-col h-full items-center justify-center">
+                <img src={emptyState} alt="Empty state" />
+                <p className="text-gray-700">
+                  Não encontramos nenhuma transação!
+                </p>
+              </div>
+            )}
+
+            {(hasTransactions && !isLoading) && (
+              <>
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="expense"/>
+
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">
+                        Almoço
+                      </strong>
+                      <span className="text-sm text-gray-600">
+                        10/10/1995
+                      </span>
+                    </div>
+                  </div>
+
+                  <span
+                    className={cn(
+                      'text-red-800 tracking-[-0.5px] font-medium',
+                      !areValuesVisible && 'blur-sm',
+                    )}
+                  >
+                    - {formatCurrency(1200)}
                   </span>
                 </div>
-              </div>
 
-              <span
-                className={cn(
-                  'text-red-800 tracking-[-0.5px] font-medium',
-                  !areValuesVisible && 'blur-sm',
-                )}
-              >
-                - {formatCurrency(1200)}
-              </span>
-            </div>
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="income"/>
 
-            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="income"/>
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">
+                        Salario
+                      </strong>
+                      <span className="text-sm text-gray-600">
+                        10/10/1995
+                      </span>
+                    </div>
+                  </div>
 
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">
-                    Salario
-                  </strong>
-                  <span className="text-sm text-gray-600">
-                    10/10/1995
+                  <span
+                    className={cn(
+                      'text-green-800 tracking-[-0.5px] font-medium',
+                      !areValuesVisible && 'blur-sm',
+                    )}
+                  >
+                    + {formatCurrency(1200)}
                   </span>
                 </div>
-              </div>
-
-              <span
-                className={cn(
-                  'text-green-800 tracking-[-0.5px] font-medium',
-                  !areValuesVisible && 'blur-sm',
-                )}
-              >
-                + {formatCurrency(1200)}
-              </span>
-            </div>
+              </>
+            )}
           </div>
         </>
       )}
